ed647b91ba87d3e742064821275605e618af6d40
[sundeo] <[sundaydeogratias8@gmail.com]>
2020-05-29 19:33:59 +0300

    More changes

--- mystore/Models/Category.cs
+++ mystore/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace mystore.Models
 {
     public class Category
     {
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public string CategoryName { get; set; }
+        [Required]
         public string Description { get; set; }
         public List<Product> Products { get; set; }
     }
--- mystore/Models/Product.cs
+++ mystore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,29 @@ namespace mystore.Models
 {
     public class Product
     {
+        [Required]
         public int ProductId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ShortDescription { get; set; }
+        [Required]
         public string LongDescription { get; set; }
+        [Required]
         public decimal Price { get; set; }
+        [Required]
+        [Url]
         public string ImageUrl { get; set; }
+        [Required]
+        [Url]
         public string ImageThumbnailUrl { get; set; }
+        [Required]
         public bool IsPreferredProduct { get; set; }
+        [Required]
         public bool InStock { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public virtual Category Category { get; set; }
     }
 }
